4075a386232c2db67ca9adfe823f72f393c9290e
Adriano Ferreira <a.r.ferreira@gmail.com>
2018-11-17 12:21:46 -0800

    Avoid warning when getting terminal size
    Latest versions of Term::Size introduced a bug fix where
    
        Term::Size::chars()
    
    returns on error an empty list on list context and undef on scalar context.
    
    This change reconciliates the code to both old and new Term::Size
    versions by using Term::Size::chars() in scalar context and testing
    $columns for falseness (rather than by equality to '').

--- TreeDumper.pm
+++ TreeDumper.pm
@@ -763,7 +763,7 @@ else
 		}
 	else
 		{
-		my ($columns, $rows) = ('', '') ;
+		my $columns = '' ;
 		
 		if(defined $setup->{WRAP_WIDTH})
 			{
@@ -775,15 +775,15 @@ else
 				{
 				if($^O ne 'MSWin32')
 					{
-					eval "(\$columns, \$rows) = Term::Size::chars *STDOUT{IO} ;" ;
+					eval '$columns = Term::Size::chars *STDOUT{IO} ;' ;
 					}
 				else
 					{
-					($columns, $rows) = $WIN32_CONSOLE->Size();
+					($columns) = $WIN32_CONSOLE->Size();
 					}
 				}
 				
-			if($columns eq '')
+			unless($columns)
 				{
 				$columns = $setup->{VIRTUAL_WIDTH}  ;
 				}
